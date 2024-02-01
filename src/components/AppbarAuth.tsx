fb0c8f61c3f72ce03733266a7e7914928f4a062b
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-02-01 22:31:46 +0530

    Update AppbarAuth.tsx

--- src/components/AppbarAuth.tsx
+++ src/components/AppbarAuth.tsx
@@ -8,7 +8,7 @@ export const AppbarAuth = () => {
   const router = useRouter();
 
   return <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 p-4">
+    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <li>
         <div onClick={() => {
           if (session.data?.user) {
